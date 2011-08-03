bc93eff1aaab1511a4b39d903b181bb10761d407
jsirois <jsirois@twitter.com>
2011-08-02 18:05:56 -0600

    Squashed commit of the following:
    commit 89463b65be3ad2d56f2810f6bef7ab07f77942a9
    Author: jsirois <jsirois@twitter.com>
    Date:   Mon Aug 1 18:07:58 2011 -0600
    
        Fixup formatting
    
    commit 494b9bdea3c8bfabf1a4d56b625a93c5e05f2804
    Author: jsirois <jsirois@twitter.com>
    Date:   Mon Aug 1 18:04:24 2011 -0600
    
        Fix missing import and build dep.
    
    commit 0e6bbb977040b35bcc9bf51d8177fced056af649
    Author: jsirois <jsirois@twitter.com>
    Date:   Mon Aug 1 17:59:04 2011 -0600
    
        Support base nonNull variants of ThriftBinaryCodec encode and decode for users that know they have non-null inputs

--- src/java/com/twitter/mesos/codec/ThriftBinaryCodec.java
+++ src/java/com/twitter/mesos/codec/ThriftBinaryCodec.java
@@ -1,13 +1,15 @@
 package com.twitter.mesos.codec;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Preconditions;
+
 import org.apache.thrift.TBase;
 import org.apache.thrift.TDeserializer;
 import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
-
-import javax.annotation.Nullable;
 
 /**
  * Codec that works for thrift objects.
@@ -23,20 +25,26 @@ public class ThriftBinaryCodec {
 
   @Nullable
   public static <T extends TBase> T decode(Class<T> clazz, byte[] buffer) throws CodingException {
-    if (buffer == null) return null;
-
-    T t;
-    try {
-      t = clazz.newInstance();
-    } catch (InstantiationException e) {
-      throw new CodingException("Failed to instantiate target type.", e);
-    } catch (IllegalAccessException e) {
-      throw new CodingException("Failed to access constructor for target type.", e);
+    if (buffer == null) {
+      return null;
     }
+    return decodeNonNull(clazz, buffer);
+  }
+
+  public static <T extends TBase> T decodeNonNull(Class<T> clazz, byte[] buffer)
+      throws CodingException {
+
+    Preconditions.checkNotNull(clazz);
+    Preconditions.checkNotNull(buffer);
 
     try {
+      T t = clazz.newInstance();
       new TDeserializer(PROTOCOL_FACTORY).deserialize(t, buffer);
       return t;
+    } catch (IllegalAccessException e) {
+      throw new CodingException("Failed to access constructor for target type.", e);
+    } catch (InstantiationException e) {
+      throw new CodingException("Failed to instantiate target type.", e);
     } catch (TException e) {
       throw new CodingException("Failed to deserialize thrift object.", e);
     }
@@ -44,7 +52,14 @@ public class ThriftBinaryCodec {
 
   @Nullable
   public static byte[] encode(TBase tBase) throws CodingException {
-    if (tBase == null) return null;
+    if (tBase == null) {
+      return null;
+    }
+    return encodeNonNull(tBase);
+  }
+
+  public static byte[] encodeNonNull(TBase tBase) throws CodingException {
+    Preconditions.checkNotNull(tBase);
 
     try {
       return new TSerializer(PROTOCOL_FACTORY).serialize(tBase);
@@ -54,9 +69,6 @@ public class ThriftBinaryCodec {
   }
 
   public static class CodingException extends Exception {
-    public CodingException(String msg) {
-      super(msg);
-    }
     public CodingException(String msg, Throwable cause) {
       super(msg, cause);
     }
