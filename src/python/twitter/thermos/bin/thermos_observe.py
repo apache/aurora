efebb1c41d4997f3ea1d030752a4d790febbb25d
Brian Wickman <wickman@twitter.com>
2011-10-04 21:23:31 -0700

    python metrics pt 4: common http library, /varz

--- src/python/twitter/thermos/bin/thermos_observe.py
+++ src/python/twitter/thermos/bin/thermos_observe.py
@@ -1,16 +1,25 @@
 import sys
+import time
 
 from twitter.common import app
-
-from twitter.thermos.observer import TaskObserver
-from twitter.thermos.observer.http import BottleObserver
+from twitter.common.metrics import RootMetrics, LambdaGauge
+from twitter.thermos.observer.observer import TaskObserver
+from twitter.thermos.observer.http     import ObserverHttpHandler
 
 app.add_option("--root", dest = "root", metavar = "DIR",
                help = "root checkpoint directory for thermos task runners")
 app.add_option("--port", dest = "port", type='int', metavar = "PORT", default=8051,
                help = "port to run observer webserver")
 
-def main(args, opts):
+def register_varz():
+  rm = RootMetrics()
+  now = time.time()
+  rm.register(LambdaGauge('uptime', lambda: time.time() - now))
+
+def main(args):
+  opts = app.get_options()
+  register_varz()
+
   if args:
     print >> sys.stderr, "ERROR: unrecognized arguments: %s\n" % (" ".join(args))
     app.help()
@@ -24,8 +33,6 @@ def main(args, opts):
   obs = TaskObserver(opts.root)
   obs.start()
 
-  # this runs forever
-  bo = BottleObserver(obs)
-  bo.run('localhost', opts.port)
+  ObserverHttpHandler('localhost', opts.port, obs)
 
 app.main()
