6a2d501e2532d11e42d17c36754886c9efd93591
Bill Farner <bill@twitter.com>
2013-06-10 14:42:54 -0700

    Make testing_log_file_path optional for now.

--- src/java/com/twitter/mesos/scheduler/log/testing/FileLogStreamModule.java
+++ src/java/com/twitter/mesos/scheduler/log/testing/FileLogStreamModule.java
@@ -2,12 +2,12 @@ package com.twitter.mesos.scheduler.log.testing;
 
 import java.io.File;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.PrivateModule;
 import com.google.inject.Singleton;
 
 import com.twitter.common.args.Arg;
 import com.twitter.common.args.CmdLine;
-import com.twitter.common.args.constraints.NotNull;
 import com.twitter.mesos.scheduler.log.Log;
 
 /**
@@ -15,12 +15,15 @@ import com.twitter.mesos.scheduler.log.Log;
  */
 public class FileLogStreamModule extends PrivateModule {
 
-  @NotNull
+  // TODO(William Farner): Make this a required argument and ensure it is not included in production
+  //                       builds (MESOS-471).
+  //@NotNull
   @CmdLine(name = "testing_log_file_path", help = "Path to a file to store local log file data in.")
   private static final Arg<File> LOG_PATH = Arg.create(null);
 
   @Override
   protected void configure() {
+    Preconditions.checkNotNull(LOG_PATH.get());
     bind(File.class).toInstance(LOG_PATH.get());
     bind(Log.class).to(FileLog.class);
     bind(FileLog.class).in(Singleton.class);
