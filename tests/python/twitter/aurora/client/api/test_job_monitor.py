598232a1d2ad5c0ec6256b80744110e9d670f537
Zameer Manji <zmanji@twitter.com>
2013-09-17 16:42:52 +0000

    Add Unit Tests for JobMonitor
    The JobMonitor class doesn't have any unit tests. This patch adds a basic test
    to prevent regressions when changing the thrift API.

---
+++ tests/python/twitter/aurora/client/api/test_job_monitor.py
@@ -0,0 +1,31 @@
+from gen.twitter.aurora.AuroraSchedulerManager import Client
+from gen.twitter.aurora.ttypes import Response, ResponseCode, Result, ScheduleStatusResult, Identity, TaskQuery
+from twitter.aurora.client.api import AuroraClientAPI
+from twitter.aurora.client.api.job_monitor import JobMonitor
+
+from mox import MoxTestBase
+
+ROLE = 'johndoe'
+ENV = 'test'
+JOB_NAME = 'test_job'
+
+
+class JobMonitorTest(MoxTestBase):
+
+  def setUp(self):
+
+    super(JobMonitorTest, self).setUp()
+    self.mock_api = self.mox.CreateMock(AuroraClientAPI)
+    self.mock_scheduler = self.mox.CreateMock(Client)
+    self.mock_api.scheduler = self.mock_scheduler
+
+  def test_init(self):
+    result = Result(scheduleStatusResult=ScheduleStatusResult(tasks=[]))
+    response = Response(responseCode=ResponseCode.OK, message="test", result=result)
+    query = TaskQuery(owner=Identity(role=ROLE), environment=ENV, jobName=JOB_NAME)
+
+    self.mock_scheduler.getTasksStatus(query).AndReturn(response)
+
+    self.mox.ReplayAll()
+
+    JobMonitor(self.mock_api, ROLE, ENV, JOB_NAME)
