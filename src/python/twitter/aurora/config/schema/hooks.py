59a0952a1f395e55e25e24430acbbf7c9015fcc6
Selvin George <sgeorge@twitter.com>
2013-09-19 21:38:50 +0000

    Aurora Client builtin hooks as schema helpers

---
+++ src/python/twitter/aurora/config/schema/hooks.py
@@ -0,0 +1,4 @@
+from twitter.aurora.client.hooks.internal.calendar import CalendarNotifier
+from twitter.aurora.client.hooks.internal.campfire import CampfireNotifier
+from twitter.aurora.client.hooks.internal.cm import CM
+from twitter.aurora.client.hooks.internal.release_db import ReleaseDB
