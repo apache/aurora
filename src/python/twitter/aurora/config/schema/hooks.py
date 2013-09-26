e4a84f8b040446b53ecf50aa3bdcbf83256f73ff
Brian Wickman <wickman@twitter.com>
2013-09-25 19:12:21 -0700

    unwire ReleaseDB from client b/c of mysql incompatibilities

--- src/python/twitter/aurora/config/schema/hooks.py
+++ src/python/twitter/aurora/config/schema/hooks.py
@@ -1,4 +1,3 @@
 from twitter.aurora.client.hooks.internal.calendar import CalendarNotifier
 from twitter.aurora.client.hooks.internal.campfire import CampfireNotifier
 from twitter.aurora.client.hooks.internal.cm import CM
-from twitter.aurora.client.hooks.internal.release_db import ReleaseDB
