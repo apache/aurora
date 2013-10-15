40e6602cd4e6e6531a4e6ee09370dce29430cfe6
Joe Smith <jsmith@twitter.com>
2013-10-15 19:54:12 +0000

    Add start maintenance command to aurora_admin.

--- src/python/twitter/aurora/admin/mesos_maintenance.py
+++ src/python/twitter/aurora/admin/mesos_maintenance.py
@@ -20,7 +20,8 @@ class MesosMaintenance(object):
   def __init__(self, cluster, verbosity):
     self._client = AuroraClientAPI(cluster, verbosity == 'verbose')
 
-  def _start_maintenance(self, drainable_hosts, clock=time):
+  def _drain_hosts(self, drainable_hosts, clock=time):
+    """This will actively turn down tasks running on hosts."""
     check_and_log_response(self._client.drain_hosts(drainable_hosts))
     not_ready_hosts = [hostname for hostname in drainable_hosts.hostNames]
     while not_ready_hosts:
@@ -38,6 +39,7 @@ class MesosMaintenance(object):
           not_ready_hosts.remove(host_status.host)
 
   def _complete_maintenance(self, drained_hosts):
+    """End the maintenance status for a give set of hosts."""
     check_and_log_response(self._client.end_maintenance(drained_hosts))
     resp = self._client.maintenance_status(drained_hosts)
     for host_status in resp.result.maintenanceStatusResult.statuses:
@@ -50,11 +52,22 @@ class MesosMaintenance(object):
       callback(host)
 
   def end_maintenance(self, hosts):
+    """Pull a list of hosts out of maintenance mode."""
     self._complete_maintenance(Hosts(set(hosts)))
 
-  def perform_maintenance(self, hosts, batch_size=0, callback=None):
-    self._complete_maintenance(Hosts(set(hosts)))
+  def start_maintenance(self, hosts):
+    """Put a list of hosts into maintenance mode, to de-prioritize scheduling."""
     check_and_log_response(self._client.start_maintenance(Hosts(set(hosts))))
+
+  def perform_maintenance(self, hosts, batch_size=0, callback=None):
+    """The wrap a callback in between sending hosts into maintenance mode and back.
+
+    Walk through the process of putting hosts into maintenance, draining them of tasks,
+    performing an action on them once drained, then removing them from maintenance mode
+    so tasks can schedule.
+    """
+    self._complete_maintenance(Hosts(set(hosts)))
+    self.start_maintenance(hosts)
     batcher = Batching()
     if batch_size > 0:
       hosts = batcher.batch_hosts_by_size(hosts, batch_size)
@@ -62,7 +75,7 @@ class MesosMaintenance(object):
       hosts = batcher.batch_hosts_by_rack(hosts)
     for batch in hosts:
       drainable_hosts = Hosts(set(batch))
-      self._start_maintenance(drainable_hosts)
+      self._drain_hosts(drainable_hosts)
       if callback:
         self._operate_on_hosts(drainable_hosts, callback)
       self._complete_maintenance(drainable_hosts)
--- tests/python/twitter/aurora/admin/test_mesos_maintenance.py
+++ tests/python/twitter/aurora/admin/test_mesos_maintenance.py
@@ -1,9 +1,18 @@
+import unittest
+
 from twitter.aurora.admin.mesos_maintenance import MesosMaintenance
 from twitter.aurora.common_internal.clusters import TWITTER_CLUSTERS
 
-from mox import MoxTestBase
+from gen.twitter.aurora.ttypes import Response, ResponseCode
+
+import mock
+
+MOCK_TEST_HOSTS = ['smf1-jim-27-sr3.prod.twitter.com']
 
 
-class TestMesosMaintenance(MoxTestBase):
-  def setUp(self):
-    self.maintenance = MesosMaintenance(TWITTER_CLUSTERS["local"], "non-verbose")
+class TestMesosMaintenance(unittest.TestCase):
+  @mock.patch("twitter.aurora.client.api.AuroraClientAPI.start_maintenance")
+  def test_start_maintenance(self, mock_api):
+    mock_api.return_value = Response(responseCode=ResponseCode.OK)
+    maintenance = MesosMaintenance(TWITTER_CLUSTERS["local"], "non-verbose")
+    maintenance.start_maintenance(MOCK_TEST_HOSTS)
