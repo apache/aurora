95a62ba5cc9af88c4bb5303f070dda410b9a0ce1
Bill Farner <bill@twitter.com>
2013-11-14 21:33:02 +0000

    Remove unused thrift field, add a few license headers.

--- src/thrift/com/twitter/aurora/gen/internal_rpc.thrift
+++ src/thrift/com/twitter/aurora/gen/internal_rpc.thrift
@@ -1,4 +1,16 @@
-// Copyright 2010 Twitter, Inc.
+// Copyright 2013 Twitter, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 namespace java com.twitter.aurora.gen.comm
 namespace py gen.twitter.aurora.comm
 
@@ -22,9 +34,8 @@ union SchedulerMessage {
 // task history associated with the host may have been purged, and the
 // executor should only retain tasks associated with the provided tasks IDs.
 struct AdjustRetainedTasks {
-  1: set<string> retainedTaskIds  // DEPRECATED - All task IDs that the executor should retain.
   2: map<string, api.ScheduleStatus> retainedTasks  // All tasks that the executor should
-                                                              // retain, and their statuses.
+                                                    // retain, and their statuses.
 }
 
 // Message sent from the scheduler to the executor.
