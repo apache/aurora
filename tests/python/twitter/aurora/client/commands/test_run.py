24a8e6cd225b84f2aa540bbbb4fdc2af78eef5b8
Mark Chu-Carroll <mchucarroll@twitter.com>
2013-11-15 18:21:41 +0000

    Add tests for aurora client "ssh" and "run" commands.

---
+++ tests/python/twitter/aurora/client/commands/test_run.py
@@ -0,0 +1,117 @@
+import contextlib
+
+from twitter.aurora.client.commands.run import run
+from twitter.aurora.client.commands.util import AuroraClientCommandTest
+
+from gen.twitter.aurora.ttypes import (
+    AssignedTask,
+    Identity,
+    JobKey,
+    ResponseCode,
+    ScheduleStatus,
+    ScheduleStatusResult,
+    TaskConfig,
+    TaskEvent,
+    TaskQuery,
+)
+
+from mock import Mock, patch
+
+
+class TestRunCommand(AuroraClientCommandTest):
+
+  @classmethod
+  def setup_mock_options(cls):
+    """set up to get a mock options object."""
+    mock_options = Mock()
+    mock_options.num_threads = 4
+    mock_options.tunnels = []
+    mock_options.executor_sandbox = False
+    mock_options.ssh_user = None
+    return mock_options
+
+  @classmethod
+  def create_mock_scheduled_tasks(cls):
+    jobs = []
+    for name in ['foo', 'bar', 'baz']:
+      job = Mock()
+      job.key = JobKey(role=cls.TEST_ROLE, environment=cls.TEST_ENV, name=name)
+      job.failure_count = 0
+      job.assignedTask = Mock(spec=AssignedTask)
+      job.assignedTask.taskId = 1287391823
+      job.assignedTask.slaveHost = 'slavehost'
+      job.assignedTask.task = Mock(spec=TaskConfig)
+      job.assignedTask.task.executorConfig = None
+      job.assignedTask.task.thermosConfig = Mock()
+      job.assignedTask.task.maxTaskFailures = 1
+      job.assignedTask.task.packages = []
+      job.assignedTask.task.owner = Identity(role='mchucarroll')
+      job.assignedTask.task.environment = 'test'
+      job.assignedTask.task.jobName = 'woops'
+      job.assignedTask.task.numCpus = 2
+      job.assignedTask.task.ramMb = 2
+      job.assignedTask.task.diskMb = 2
+      job.assignedTask.instanceId = 4237894
+      job.assignedTask.assignedPorts = {}
+      job.status = ScheduleStatus.RUNNING
+      mockEvent = Mock(spec=TaskEvent)
+      mockEvent.timestamp = 28234726395
+      mockEvent.status = ScheduleStatus.RUNNING
+      mockEvent.message = "Hi there"
+      job.taskEvents = [mockEvent]
+      jobs.append(job)
+    return jobs
+
+  @classmethod
+  def create_status_response(cls):
+    resp = cls.create_simple_success_response()
+    resp.result.scheduleStatusResult = Mock(spec=ScheduleStatusResult)
+    resp.result.scheduleStatusResult.tasks = cls.create_mock_scheduled_tasks()
+    return resp
+
+  @classmethod
+  def create_failed_status_response(cls):
+    return cls.create_blank_response(ResponseCode.INVALID_REQUEST, 'No tasks found for query')
+
+  @classmethod
+  def create_mock_process(cls):
+    process = Mock()
+    process.communicate.return_value = ["hello", "world"]
+    return process
+
+  def test_successful_run(self):
+    """Test the run command."""
+    # Calls api.check_status, which calls scheduler.getJobs
+    mock_options = self.setup_mock_options()
+    (mock_api, mock_scheduler) = self.create_mock_api()
+    mock_scheduler.getTasksStatus.return_value = self.create_status_response()
+    sandbox_args = {'slave_root': '/slaveroot', 'slave_run_directory': 'slaverun'}
+    with contextlib.nested(
+        patch('twitter.aurora.client.api.SchedulerProxy', return_value=mock_scheduler),
+        patch('twitter.aurora.client.factory.CLUSTERS', new=self.TEST_CLUSTERS),
+        patch('twitter.aurora.client.commands.run.CLUSTERS', new=self.TEST_CLUSTERS),
+        patch('twitter.common.app.get_options', return_value=mock_options),
+        patch('twitter.aurora.client.api.command_runner.DistributedCommandRunner.sandbox_args',
+            return_value=sandbox_args),
+        patch('subprocess.Popen', return_value=self.create_mock_process())) as (
+            mock_scheduler_proxy_class,
+            mock_clusters,
+            mock_clusters_runpatch,
+            options,
+            mock_runner_args_patch,
+            mock_subprocess):
+      run(['west/mchucarroll/test/hello', 'ls'], mock_options)
+
+      # The status command sends a getTasksStatus query to the scheduler,
+      # and then prints the result.
+      mock_scheduler.getTasksStatus.assert_called_with(TaskQuery(jobName='hello',
+          environment='test', owner=Identity(role='mchucarroll'),
+          statuses=set([ScheduleStatus.RUNNING, ScheduleStatus.KILLING, ScheduleStatus.RESTARTING,
+              ScheduleStatus.PREEMPTING, ScheduleStatus.UPDATING, ScheduleStatus.ROLLBACK])))
+
+      # The mock status call returns 3 three ScheduledTasks, so three commands should have been run
+      assert mock_subprocess.call_count == 3
+      mock_subprocess.assert_called_with(['ssh', '-n', '-q', 'mchucarroll@slavehost',
+          'cd /slaveroot/slaves/*/frameworks/*/executors/thermos-1287391823/runs/'
+          'slaverun/sandbox;ls'],
+          stderr=-2, stdout=-1)
---
+++ tests/python/twitter/aurora/client/commands/test_ssh.py
@@ -0,0 +1,104 @@
+import contextlib
+
+from twitter.aurora.client.commands.ssh import ssh
+from twitter.aurora.client.commands.util import AuroraClientCommandTest
+
+from gen.twitter.aurora.ttypes import (
+    AssignedTask,
+    Identity,
+    JobKey,
+    ResponseCode,
+    ScheduleStatus,
+    ScheduleStatusResult,
+    TaskConfig,
+    TaskEvent,
+    TaskQuery,
+)
+
+from mock import Mock, patch
+
+
+class TestSshCommand(AuroraClientCommandTest):
+
+  @classmethod
+  def setup_mock_options(cls):
+    """set up to get a mock options object."""
+    mock_options = Mock()
+    mock_options.tunnels = []
+    mock_options.executor_sandbox = False
+    mock_options.ssh_user = None
+    return mock_options
+
+  @classmethod
+  def create_mock_scheduled_tasks(cls):
+    jobs = []
+    for name in ['foo', 'bar', 'baz']:
+      job = Mock()
+      job.key = JobKey(role=cls.TEST_ROLE, environment=cls.TEST_ENV, name=name)
+      job.failure_count = 0
+      job.assignedTask = Mock(spec=AssignedTask)
+      job.assignedTask.taskId = 1287391823
+      job.assignedTask.slaveHost = 'slavehost'
+      job.assignedTask.task = Mock(spec=TaskConfig)
+      job.assignedTask.task.executorConfig = None
+      job.assignedTask.task.thermosConfig = Mock()
+      job.assignedTask.task.maxTaskFailures = 1
+      job.assignedTask.task.packages = []
+      job.assignedTask.task.owner = Identity(role='mchucarroll')
+      job.assignedTask.task.environment = 'test'
+      job.assignedTask.task.jobName = 'woops'
+      job.assignedTask.task.numCpus = 2
+      job.assignedTask.task.ramMb = 2
+      job.assignedTask.task.diskMb = 2
+      job.assignedTask.instanceId = 4237894
+      job.assignedTask.assignedPorts = {}
+      job.status = ScheduleStatus.RUNNING
+      mockEvent = Mock(spec=TaskEvent)
+      mockEvent.timestamp = 28234726395
+      mockEvent.status = ScheduleStatus.RUNNING
+      mockEvent.message = "Hi there"
+      job.taskEvents = [mockEvent]
+      jobs.append(job)
+    return jobs
+
+  @classmethod
+  def create_status_response(cls):
+    resp = cls.create_simple_success_response()
+    resp.result.scheduleStatusResult = Mock(spec=ScheduleStatusResult)
+    resp.result.scheduleStatusResult.tasks = cls.create_mock_scheduled_tasks()
+    return resp
+
+  @classmethod
+  def create_failed_status_response(cls):
+    return cls.create_blank_response(ResponseCode.INVALID_REQUEST, 'No tasks found for query')
+
+  def test_successful_ssh(self):
+    """Test the ssh command."""
+    # Calls api.check_status, which calls scheduler.getJobs
+    mock_options = self.setup_mock_options()
+    (mock_api, mock_scheduler) = self.create_mock_api()
+    mock_scheduler.getTasksStatus.return_value = self.create_status_response()
+    sandbox_args = {'slave_root': '/slaveroot', 'slave_run_directory': 'slaverun'}
+    with contextlib.nested(
+        patch('twitter.aurora.client.api.SchedulerProxy', return_value=mock_scheduler),
+        patch('twitter.aurora.client.factory.CLUSTERS', new=self.TEST_CLUSTERS),
+        patch('twitter.common.app.get_options', return_value=mock_options),
+        patch('twitter.aurora.client.api.command_runner.DistributedCommandRunner.sandbox_args',
+            return_value=sandbox_args),
+        patch('subprocess.call', return_value=0)) as (
+            mock_scheduler_proxy_class,
+            mock_clusters,
+            options,
+            mock_runner_args_patch,
+            mock_subprocess):
+      ssh(['west/mchucarroll/test/hello', '1', 'ls'], mock_options)
+
+      # The status command sends a getTasksStatus query to the scheduler,
+      # and then prints the result.
+      mock_scheduler.getTasksStatus.assert_called_with(TaskQuery(jobName='hello',
+          environment='test', owner=Identity(role='mchucarroll'), instanceIds=set([1]),
+          statuses=set([ScheduleStatus.RUNNING, ScheduleStatus.KILLING, ScheduleStatus.RESTARTING,
+              ScheduleStatus.PREEMPTING, ScheduleStatus.UPDATING, ScheduleStatus.ROLLBACK])))
+      mock_subprocess.assert_called_with(['ssh', '-t', 'mchucarroll@slavehost',
+          'cd /slaveroot/slaves/*/frameworks/*/executors/thermos-1287391823/runs/'
+          'slaverun/sandbox;ls'])
