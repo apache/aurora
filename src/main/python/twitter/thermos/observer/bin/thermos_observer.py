0db4787b2617fb3404b95b82b0840a204cee6b29
Brian Wickman <wickman@twitter.com>
2013-11-19 16:24:26 -0800

    make observer work w/o rootserver module

--- src/main/python/twitter/thermos/observer/bin/thermos_observer.py
+++ src/main/python/twitter/thermos/observer/bin/thermos_observer.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
+
 import socket
 import sys
 import time
 
 from twitter.common import app
-from twitter.common.app.modules.http import RootServer
+from twitter.common.exceptions import ExceptionalThread
+from twitter.common.http import HttpServer
+from twitter.common.http.diagnostics import DiagnosticsEndpoints
 from twitter.thermos.common.path import TaskPath
 from twitter.thermos.observer.task_observer import TaskObserver
 from twitter.thermos.observer.http.http_observer import BottleObserver
@@ -16,26 +20,36 @@ app.add_option("--root",
                help="root checkpoint directory for thermos task runners")
 
 
-app.configure(module='twitter.common.app.modules.http',
-    port=1338, host='0.0.0.0', enable=True, framework='cherrypy')
+app.add_option("--port",
+               dest="port",
+               metavar="INT",
+               default=1338,
+               help="port number to listen on.")
 
 
 def main(args, opts):
   if args:
-    print >> sys.stderr, "ERROR: unrecognized arguments: %s\n" % (" ".join(args))
+    print("ERROR: unrecognized arguments: %s\n" % (" ".join(args)), file=sys.stderr)
     app.help()
     sys.exit(1)
+
+  root_server = HttpServer()
+  root_server.mount_routes(DiagnosticsEndpoints())
 
   task_observer = TaskObserver(opts.root)
   task_observer.start()
 
   bottle_wrapper = BottleObserver(task_observer)
-  RootServer().mount_routes(bottle_wrapper)
 
-  # MainThread should just sleep forever.  TODO(wickman) Add something in
-  # app that indicates sleeping forever until e.g.  a signal.
-  while True:
-    time.sleep(10)
+  root_server.mount_routes(bottle_wrapper)
+
+  def run():
+    root_server.run('0.0.0.0', opts.port, 'cherrypy')
+
+  et = ExceptionalThread(target=run)
+  et.daemon = True
+  et.start()
+  et.join()
 
 
 app.main()
