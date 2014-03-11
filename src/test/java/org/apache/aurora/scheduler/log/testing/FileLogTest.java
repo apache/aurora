9642eee84cc9bbcbd62e24c567c66ed686ba1bd8
Jake Farrell <jfarrell@apache.org>
2014-03-10 22:05:32 -0400

    Aurora-102: Add failOnVersionConfict() to scheduler build
    Adds resolution strategy to compile time to detect and fail if any dependency
    versions have conflicts.
    
    Testing Done:
    ran local build with protobuf dependency set to 2.4.1
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Could not resolve all dependencies for configuration ':compile'.
    > A conflict was found between the following modules:
       - com.google.protobuf:protobuf-java:2.4.1
       - com.google.protobuf:protobuf-java:2.5.0
    
    changed to version 2.5.0
    
    BUILD SUCCESSFUL
    
    Bugs closed: AURORA-102
    
    Reviewed at https://reviews.apache.org/r/18926/

--- src/test/java/org/apache/aurora/scheduler/log/testing/FileLogTest.java
+++ src/test/java/org/apache/aurora/scheduler/log/testing/FileLogTest.java
@@ -24,7 +24,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 
 public class FileLogTest {
 
