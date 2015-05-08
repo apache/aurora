3247a6c94091406367fce2efb8c6f8bdda853b9a
Jake Farrell <jfarrell@apache.org>
2015-05-07 23:38:33 -0400

    Fix broken style test cases causing PEP302 errors
    Testing Done:
    all tests including check style pass with this patch now
    
    Bugs closed: AURORA-1313
    
    Reviewed at https://reviews.apache.org/r/33969/

--- src/test/python/apache/aurora/executor/bin/test_thermos_executor_entry_point.py
+++ src/test/python/apache/aurora/executor/bin/test_thermos_executor_entry_point.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 #
 
+
 def test_thermos_executor_valid_import_dependencies():
   from apache.aurora.executor.bin.thermos_executor_main import proxy_main
   assert proxy_main is not None
