a4e7b3a43c92e97f7aa24c87a47bfb2dfcdd50cc
Zameer Manji <zmanji@apache.org>
2016-03-04 12:46:13 -0500

    Install thrift package from the offical debs.
    `twopercent` pointed out on IRC that due to the people.a.o change our vagrant
    provisioning was broken. This ports the change from aurora-packaging to our
    vagrant script.
    
    Reviewed at https://reviews.apache.org/r/44347/

--- examples/vagrant/provision-dev-cluster.sh
+++ examples/vagrant/provision-dev-cluster.sh
@@ -46,10 +46,11 @@ function prepare_extras() {
     popd
 
     # Install thrift, needed for code generation in the scheduler build.
-    # TODO(wfarner): Move deb file out of jfarrell's individual hosting.
-    thrift_deb=thrift-compiler_0.9.1_amd64.deb
-    wget -c http://people.apache.org/~jfarrell/thrift/0.9.1/contrib/deb/ubuntu/12.04/$thrift_deb
-    dpkg --install $thrift_deb
+    curl -sSL http://apache.org/dist/thrift/KEYS | gpg --import -
+    gpg --export --armor 66B778F9 | sudo apt-key add -
+    echo 'deb http://www.apache.org/dist/thrift/debian 0.9.1 main' > /etc/apt/sources.list.d/thrift.list
+    apt-get update
+    apt-get install thrift-compiler=0.9.1
 
     # Include build script in default PATH.
     ln -sf /home/vagrant/aurora/examples/vagrant/aurorabuild.sh /usr/local/bin/aurorabuild
