3e239b48becb9359850c7f016ba7ab67aa6a68bf
David McLaughlin <david@dmclaughlin.com>
2017-10-16 16:31:34 -0700

    Add support for controlling API url in UI without modifying code.
    Reviewed at https://reviews.apache.org/r/63040/

--- ui/src/main/js/client/scheduler-client.js
+++ ui/src/main/js/client/scheduler-client.js
@@ -1,5 +1,21 @@
+import { isNully } from 'utils/Common';
+
+function getBaseUrl() {
+  // Power feature: load a different Scheduler URL from local storage to test against live APIs.
+  //
+  // In your browser console:
+  //    window.localStorage.apiUrl = 'http://myauroracluster.com';
+  //
+  // To remove the item:
+  //    window.localStorage.removeItem('apiUrl')
+  if (window && window.localStorage) {
+    return isNully(window.localStorage.apiUrl) ? '' : window.localStorage.apiUrl;
+  }
+  return '';
+}
+
 function makeClient() {
-  const transport = new Thrift.Transport('/api');
+  const transport = new Thrift.Transport(`${getBaseUrl()}/api`);
   const protocol = new Thrift.Protocol(transport);
   return new ReadOnlySchedulerClient(protocol);
 }
